abc5e3ebd23614cf324cda177b57686cfb6ce3a9
Arin <arinyazilim@gmail.com>
2021-04-14 01:38:11 +0300

    Template Engine added

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
 //MIDDLEWARES
 app.use(express.static('public'));
 
+//ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 const port = 3000;
