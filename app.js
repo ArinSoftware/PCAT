5d4af68ee61621869d54ef32eea3661aefe2471d
Arin <arinyazilim@gmail.com>
2021-04-13 16:21:24 +0300

    Express - Nodemon Installed

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+  const photo = {
+    id: 1,
+    name: "Photo Name",
+    description: "Photo description"
+  }
+  res.send(photo)
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
