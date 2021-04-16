00b97493486d994a625c5aeab6d3820ca89f773b
Arin <arinyazilim@gmail.com>
2021-04-17 00:22:36 +0300

    Add Update - Req Body

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 //ROUTES
 app.get('/', (req, res) => {
@@ -20,6 +22,13 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
+
+app.post('/photos', (req, res) => {
+  console.log(req.body);
+  res.redirect('/')
+});
+
+
 
 const port = 3000;
 app.listen(port, () => {
