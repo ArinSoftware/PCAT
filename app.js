5fd759b5622cb7f1a521b56b4f2137383ec0e20b
Arin <arinyazilim@gmail.com>
2021-04-17 11:54:46 +0300

    Model and Dynamic Content

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -13,9 +21,13 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
 });
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
@@ -23,8 +35,8 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.post('/photos', (req, res) => {
-  console.log(req.body);
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
   res.redirect('/')
 });
 
