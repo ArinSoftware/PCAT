6b95ce6245a3140b0714c7c59ac20d0a782fe96f
Arin <arinyazilim@gmail.com>
2021-04-21 00:44:08 +0300

    Pagination

--- controllers/photoControllers.js
+++ controllers/photoControllers.js
@@ -2,10 +2,28 @@ const Photo = require('../models/Photo');
 const fs = require('fs');
 
 exports.getAllPhotos = async (req, res) => {
+
+  const page = req.query.page || 1;
+  const photosPerPage = 3;
+
+  const totalPhotos = await Photo.find().countDocuments();
+
+  const photos = await Photo.find({})
+  .sort('-dateCreated')
+  .skip((page-1) * photosPerPage)
+  .limit(photosPerPage)
+
+  res.render('index', {
+    photos: photos,
+    current: page,
+    pages: Math.ceil(totalPhotos / photosPerPage)
+  });
+
+/*   console.log(req.query);
   const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', {
     photos,
-  });
+  }); */
 };
 
 exports.getPhoto = async (req, res) => {
